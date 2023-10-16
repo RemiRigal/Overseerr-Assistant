6912b8dbfa8a17e0e3b60affb7f2546114deb465
val0n <90992974+val0n@users.noreply.github.com>
2023-10-16 18:18:47 +0300

    Update IMDb anchor element

--- js/content-scripts/imdb.js
+++ js/content-scripts/imdb.js
@@ -1,6 +1,6 @@
 let overseerrContainer, imdbId, tmdbId, mediaType, mediaInfo;
 
-containerOptions.anchorElement = `div.sc-7383924-5`;
+containerOptions.anchorElement = `div.sc-dffc6c81-5`;
 containerOptions.textClass = 'oa-text-sm';
 containerOptions.containerClass = 'oa-mt-2 oa-py-2';
 containerOptions.badgeBackground = '#313131';
